3ad20dc51c64b3b0fa4049578bc602d7c59d2a56
Mitchell Thompson <mitchellth86@gmail.com>
2026-03-29 15:34:12 -0700

    updated versions

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0"
+      version = "~> 3.1.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 3.0"
+      version = "~> 3.0.1"
     }
   }
 }
